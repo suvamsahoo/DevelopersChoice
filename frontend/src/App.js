0a6164f25ca6657f9ac17c194f712251cb4254c9
suvamsahoo <suvamsahoo77@gmail.com>
2021-03-12 04:36:46 +0530

    UC4: Used map() for products and stored all items data inside data.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,149 +1,62 @@
 import React from "react";
+import data from './data'
 
 function App() {
   return (
     <div className="grid-container">
       <header className="row">
         <div>
-          <a className="brand" href="index.html">
+          <a className="brand" href="/">
             DeveloperChoice
           </a>
         </div>
         <div>
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
         </div>
       </header>
 
       <main>
         <div className="row center">
-          <div className="card">
-            <a href="product.html">
-              <img className="medium" src="./images/react.jpg" alt="product" />
-            </a>
-
-            <div className="card-body">
-              <a href="product.html">
-                <h2>React T-Shirt</h2>
+          
+          {data.products.map((product) => (
+            <div key={product._id} className="card">
+              <a href={`/product/${product._id}`}>
+                <img
+                  className="medium"
+                  src={product.image}
+                  alt={product.name}
+                />
               </a>
 
-              <div className="rating">
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
+              <div className="card-body">
+                <a href={`/product/${product._id}`}>
+                  <h2>{product.name}</h2>
+                </a>
+
+                <div className="rating">
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                </div>
+
+                <div className="price">₹{product.price}</div>
               </div>
-
-              <div className="price">₹499</div>
             </div>
-          </div>
+          ))}
 
-          <div className="card">
-            <a href="product.html">
-              <img className="medium" src="./images/react.jpg" alt="product" />
-            </a>
-
-            <div className="card-body">
-              <a href="product.html">
-                <h2>React T-Shirt</h2>
-              </a>
-
-              <div className="rating">
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-              </div>
-
-              <div className="price">₹499</div>
-            </div>
-          </div>
-
-          <div className="card">
-            <a href="product.html">
-              <img className="medium" src="./images/react.jpg" alt="product" />
-            </a>
-
-            <div className="card-body">
-              <a href="product.html">
-                <h2>React T-Shirt</h2>
-              </a>
-
-              <div className="rating">
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-              </div>
-
-              <div className="price">₹499</div>
-            </div>
-          </div>
-
-          <div className="card">
-            <a href="product.html">
-              <img className="medium" src="./images/react.jpg" alt="product" />
-            </a>
-
-            <div className="card-body">
-              <a href="product.html">
-                <h2>React T-Shirt</h2>
-              </a>
-
-              <div className="rating">
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-              </div>
-
-              <div className="price">₹499</div>
-            </div>
-          </div>
         </div>
       </main>
 
---
+++ frontend/src/data.js
@@ -0,0 +1,83 @@
+const data = {
+    products: [
+        {
+            _id:"1",
+            name:"React Half Sleeve",
+            category:"T-Shirt",
+            image:"/images/react.jpg",
+            price:499,
+            brand: "React",
+            rating:4.9,
+            numReviews:10,
+            description:"Classic, lightweight jersey fabric comprising 100% cotton with ribbed knit crew neck."
+        },
+        {
+            _id:"2",
+            name:"Python Half Sleeve",
+            category:"T-Shirt",
+            image:"/images/python.jpg",
+            price:499,
+            brand: "Python",
+            rating:4.5,
+            numReviews:19,
+            description:"Classic, lightweight jersey fabric comprising 100% cotton with ribbed knit crew neck."
+        },
+        {
+            _id:"3",
+            name:"Google Cloud",
+            category:"Hoodie",
+            image:"/images/google.jpg",
+            price:799,
+            brand: "Google",
+            rating:4.3,
+            numReviews:49,
+            description:"Classic, lightweight jersey fabric comprising 100% cotton with ribbed knit crew neck."
+        },
+        {
+            _id:"4",
+            name:"AWS Black Boxer",
+            category:"Shorts",
+            image:"/images/aws.jpg",
+            price:399,
+            brand: "Amazon",
+            rating:4.6,
+            numReviews:87,
+            description:"Classic, lightweight jersey fabric comprising 100% cotton with ribbed knit crew neck."
+        },
+        {
+            _id:"5",
+            name:"Microsoft Polo",
+            category:"T-Shirt",
+            image:"/images/microsoft.jpg",
+            price:699,
+            brand: "Microsoft",
+            rating:4.9,
+            numReviews:24,
+            description:"Classic, lightweight jersey fabric comprising 100% cotton with ribbed knit crew neck."
+        },
+        {
+            _id:"6",
+            name:"Java Full Sleeve",
+            category:"T-Shirt",
+            image:"/images/java.jpg",
+            price:499,
+            brand: "Java",
+            rating:4.4,
+            numReviews:91,
+            description:"Classic, lightweight jersey fabric comprising 100% cotton with ribbed knit crew neck."
+        },
+        {
+            _id:"7",
+            name:"JavaScript Geek Hoodies",
+            category:"Hoodie",
+            image:"/images/javascript.jpg",
+            price:799,
+            brand: "JavaScript",
+            rating:5,
+            numReviews:12,
+            description:"Classic, lightweight jersey fabric comprising 100% cotton with ribbed knit crew neck."
+        },
+    ]
+}
+
+export default data;
