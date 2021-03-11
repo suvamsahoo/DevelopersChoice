fae4914e1d294a4375510e1ab23755e11df12c90
suvamsahoo <suvamsahoo77@gmail.com>
2021-03-12 03:30:11 +0530

    UC3: create React app and take template file code (html & css)

---
+++ frontend/src/App.js
@@ -0,0 +1,155 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="index.html">
+            DeveloperChoice
+          </a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
+      </header>
+
+      <main>
+        <div className="row center">
+          <div className="card">
+            <a href="product.html">
+              <img className="medium" src="./images/react.jpg" alt="product" />
+            </a>
+
+            <div className="card-body">
+              <a href="product.html">
+                <h2>React T-Shirt</h2>
+              </a>
+
+              <div className="rating">
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+              </div>
+
+              <div className="price">₹499</div>
+            </div>
+          </div>
+
+          <div className="card">
+            <a href="product.html">
+              <img className="medium" src="./images/react.jpg" alt="product" />
+            </a>
+
+            <div className="card-body">
+              <a href="product.html">
+                <h2>React T-Shirt</h2>
+              </a>
+
+              <div className="rating">
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+              </div>
+
+              <div className="price">₹499</div>
+            </div>
+          </div>
+
+          <div className="card">
+            <a href="product.html">
+              <img className="medium" src="./images/react.jpg" alt="product" />
+            </a>
+
+            <div className="card-body">
+              <a href="product.html">
+                <h2>React T-Shirt</h2>
+              </a>
+
+              <div className="rating">
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+              </div>
+
+              <div className="price">₹499</div>
+            </div>
+          </div>
+
+          <div className="card">
+            <a href="product.html">
+              <img className="medium" src="./images/react.jpg" alt="product" />
+            </a>
+
+            <div className="card-body">
+              <a href="product.html">
+                <h2>React T-Shirt</h2>
+              </a>
+
+              <div className="rating">
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+                <span>
+                  <i className="fa fa-star"></i>
+                </span>
+              </div>
+
+              <div className="price">₹499</div>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <footer className="row center">All right reserved</footer>
+    </div>
+  );
+}
+
+export default App;
