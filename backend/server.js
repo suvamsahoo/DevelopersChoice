c72d95dc1d58bb9d21ef91ef478e33a8cbd2a94d
suvamsahoo <suvamsahoo77@gmail.com>
2021-03-14 16:11:16 +0530

    UC15: connect with mongoDB & create DB, insert two users documents to the collections

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 import express from "express";
+import mongoose from 'mongoose';
 const app = express();
+mongoose.connect(
+  'mongodb://localhost/DevelopersChoice',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+
 import data from "./data.js";
+import userRouter from "./routers/userRouters.js";
 
 //To get product data-:
 app.get("/api/products/:id", (req, res) => {
@@ -17,8 +26,15 @@ app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
 
+app.use('/api/users', userRouter);
+
 app.get("/", (req, res) => {
   res.send("server is ready");
+});
+
+//This middleware used for error catcher-:
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
