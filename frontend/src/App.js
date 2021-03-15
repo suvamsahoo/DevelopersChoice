c0d15c763db19b1f52b1e5f568a87f3db30b9c41
suvamsahoo <suvamsahoo77@gmail.com>
2021-03-15 13:43:10 +0530

    UC20: added RegisterScreen in the route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import CartScreen from "./screens/CartScreen";
 import { useSelector, useDispatch } from "react-redux";
 import SigninScreen from "./screens/SigninScreen";
 import { signout } from "./actions/userActions";
+import RegisterScreen from "./screens/RegisterScreen";
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -59,6 +60,7 @@ function App() {
           <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/signin" component={SigninScreen}></Route>
+          <Route path="/register" component={RegisterScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
 
