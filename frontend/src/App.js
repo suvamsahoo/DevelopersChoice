72c992b4928f3bbcfd94e0934d4f5300d374dda7
suvamsahoo <suvamsahoo77@gmail.com>
2021-03-15 15:02:12 +0530

    UC21: added ShippingScreen in the route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import { useSelector, useDispatch } from "react-redux";
 import SigninScreen from "./screens/SigninScreen";
 import { signout } from "./actions/userActions";
 import RegisterScreen from "./screens/RegisterScreen";
+import ShippingAddressScreen from "./screens/ShippingAddressScreen";
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -61,6 +62,7 @@ function App() {
           <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/signin" component={SigninScreen}></Route>
           <Route path="/register" component={RegisterScreen}></Route>
+          <Route path="/shipping" component={ShippingAddressScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
 
