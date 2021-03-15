81f2aeea33a7048844882dfed89d63b7c9029f53
suvamsahoo <suvamsahoo77@gmail.com>
2021-03-15 10:40:55 +0530

    UC19: store userSignin in redux store, use localStorage for If we refresh the page then user signin data will get from localStorage

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,8 +2,16 @@ import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import { cartReducer } from "./reducers/cartReducers";
 import { productDetailsReducer, productListReducer } from "./reducers/productReducers";
+import { userSigninReducer } from "./reducers/userReducers";
 
 const initialState = {
+  //If we refresh the page data will get from localStorage-:
+  userSignin: {
+    userInfo: localStorage.getItem('userInfo')
+      ? JSON.parse(localStorage.getItem('userInfo'))
+      : null,
+  },
+
   cart: {
     cartItems: localStorage.getItem('cartItems')
       ? JSON.parse(localStorage.getItem('cartItems'))
@@ -16,6 +24,7 @@ const reducer =  combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userSignin: userSigninReducer,
 })
 
 //For connect between redux store & Redux DevTools Extension-: 
