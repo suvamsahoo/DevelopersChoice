19a4e51a2a7c08afaf51a09c1bb815a0edeebaa4
suvamsahoo <suvamsahoo77@gmail.com>
2021-03-15 13:03:59 +0530

    UC20: create an API to register user

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -39,4 +39,23 @@ userRouter.post(
   })
 );
 
+/* http://localhost:5000/api/users/register in this API we can create user */
+userRouter.post(
+  '/register',
+  expressAsyncHandler(async (req, res) => {
+    const user = new User({//User model
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 8),
+    });
+    const createdUser = await user.save();
+    res.send({
+      _id: createdUser._id,
+      name: createdUser.name,
+      email: createdUser.email,
+      isAdmin: createdUser.isAdmin,
+      token: generateToken(createdUser),
+    });
+  })
+);
 export default userRouter;
